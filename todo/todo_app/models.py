1cd120f787deed5e9027032d35d40adad1a24e8a
Basit Ali <37873801+iambasitali@users.noreply.github.com>
2021-09-08 02:34:32 +0500

    django_object_edit_form
    django_object_edit_form

--- todo/todo_app/models.py
+++ todo/todo_app/models.py
@@ -1,17 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    is_delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    modified_at = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        self.modified_at = timezone.now()
+        return super(Category, self).save(*args, **kwargs)
+
 
 class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(max_length=50)
+    due_date = models.DateTimeField(auto_now_add=True,null=True)
+    is_delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    modified_at = models.DateTimeField(auto_now_add=True,null=True) 
 
     def __str__(self):
-        return self.text
+        return self.name
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        self.modified_at = timezone.now()
+        return super(Item, self).save(*args, **kwargs)
